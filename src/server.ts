d4efbc8867226deef35d26e14cf215aee6fbeed9
ValentinLorand <val.lorand@hotmail.fr~>
2021-10-17 15:05:01 +0200

    Add socket and static access to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import * as log from "https://deno.land/std@0.111.0/log/mod.ts";
-import {Application} from 'https://deno.land/x/oak@v6.2.0/mod.ts'; //Dernière version 9.0.0 mais ne fonctionne pas
+import {Application,Router,RouterContext,send} from 'https://deno.land/x/oak@v6.2.0/mod.ts'; //Dernière version 9.0.0 mais ne fonctionne pas
 import {viewEngine, engineFactory, adapterFactory } from 'https://deno.land/x/view_engine@v1.4.5/mod.ts';
+import { WebSocketClient, WebSocketServer } from "https://deno.land/x/websocket@v0.1.3/mod.ts";
 
 // Setup engine
 const ejsEngine = engineFactory.getEjsEngine();
@@ -10,11 +11,37 @@ const oakAdapter = adapterFactory.getOakAdapter();
 const app = new Application();
 app.use(viewEngine(oakAdapter, ejsEngine));
 
-// Define the templace to use
-app.use((ctx) => {
-  ctx.render(`${Deno.cwd()}/static/index.ejb`, {data: {msg: 'Tips'}});
+
+// Handlers
+function handleIndex(ctx:RouterContext){
+  ctx.render(`${Deno.cwd()}/public/views/index.ejs`, {data: {nom: 'Léo'}});
+}
+
+
+// Routes
+const router = new Router();
+router.get('/',handleIndex);
+// router.get('/static/:path+',handleStatic);
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.use(async (context) => {
+  await send(context, context.request.url.pathname, {
+    root: `${Deno.cwd()}/public`,
+  });
+});
+
+
+//Start socket tunnel
+const wss = new WebSocketServer(8080);
+wss.on("connection", function (ws: WebSocketClient) {
+  ws.on("message", function (message: string) {
+    console.log(message);
+    ws.send("Bonjour à toi client");
+  });
 });
 
 // Start application
 log.info("Démarrage du serveur sur le port 8000...")
-await app.listen({port: 8000});  
+await app.listen({port: 8000});
