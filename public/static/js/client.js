eec456d69323cb4a02d408f1e088fc6d2657fe98
ValentinLorand <val.lorand@hotmail.fr~>
2021-10-17 15:04:39 +0200

    Move statics and views en public folder

---
+++ public/static/js/client.js
@@ -0,0 +1,24 @@
+const socket = new WebSocket("ws://127.0.0.1:8080");
+
+socket.onopen = function(e) {
+  alert("[open] Connection established");
+  socket.send("Bonjour à toi serveur");
+};
+
+socket.onmessage = function(event) {
+  alert(`[message] Data received from server: ${event.data}`);
+};
+
+socket.onclose = function(event) {
+  if (event.wasClean) {
+    alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+  } else {
+    // e.g. server process killed or network down
+    // event.code is usually 1006 in this case
+    alert('[close] Connection died');
+  }
+};
+
+socket.onerror = function(error) {
+  alert(`[error] ${error.message}`);
+};
