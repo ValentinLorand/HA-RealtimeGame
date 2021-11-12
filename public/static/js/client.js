bdc982cecca8bb9ecfe52dc9d7ecaa9209bd2d3b
Léo <rolland.leo@orange.fr>
2021-11-12 21:32:38 +0000

    feat(front): save secret in cookie and re-login at reload

--- public/static/js/client.js
+++ public/static/js/client.js
@@ -6,26 +6,48 @@ if (window.location.hostname.includes("8000")) {
   // If ran as a regular server, websocket is on same hostname, unsecure, port 8080
   websocketURI = `ws://${window.location.hostname}:8080`
 }
-const socket = new WebSocket(websocketURI);
 
-socket.onopen = function(e) {
-  console.log("[open] Connection established");
-  socket.send("join_game");
-};
+function joinGame($event) {
 
-socket.onmessage = function(event) {
-  console.log(`[message] Data received from server: ${event.data}`);
-  render(JSON.parse(event.data))
-};
+  let nickname = getCookie("nickname")
+  let secret = getCookie("secret")
 
-socket.onclose = function(event) {
-  if (event.wasClean) {
-    console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-  } else {
-    // e.g. server process killed or network down
-    // event.code is usually 1006 in this case
-    console.error('[close] Connection died');
-  }
-};
+  const socket = new WebSocket(websocketURI);
 
-socket.onerror = console.error
+  socket.onopen = function(e) {
+
+    if (secret != "" && nickname != "") {
+      console.log(`Found cookie, using nickname ${nickname} and secret ${secret}`)
+      socket.send(`get_state ${secret}`)
+    } else {
+      console.log(`No previous session found, creating one...`)
+      nickname = document.getElementById("nickname").value.trim()
+      setCookie("nickname", nickname)
+      secret = makeid(10)
+      setCookie("secret", secret)
+      socket.send(`join_game ${nickname} ${secret}`);
+      console.log(`[open] Session created. Game joined using nickname : ${nickname} secret : ${secret}`);
+    }
+
+  };
+  
+  socket.onmessage = function(event) {
+    console.log(`[message] Data received from server: ${event.data}`);
+    render(JSON.parse(event.data))
+  };
+  
+  socket.onclose = function(event) {
+    if (event.wasClean) {
+      console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    } else {
+      // e.g. server process killed or network down
+      // event.code is usually 1006 in this case
+      console.error('[close] Connection died');
+    }
+  };
+
+  socket.onerror = console.error
+
+}
+
+if (getCookie("nickname") && getCookie("secret")) joinGame()
---
+++ public/static/js/cookieHelper.js
@@ -0,0 +1,37 @@
+// FROM https://www.w3schools.com/js/js_cookies.asp
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+/**
+ * Generate a random string ID
+ * @param {number} length Length of id
+ */
+function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() *  charactersLength));
+  }
+  return result;
+}
